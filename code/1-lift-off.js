061be0648091b0701a0fc19a8d324fa76c25e6ff
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-10 05:03:42 +0000

    did the lift off task

--- code/1-lift-off.js
+++ code/1-lift-off.js
@@ -3,7 +3,7 @@
 // ####################################
 
 function liftOff() {
-  let count = 10
+  let count = prompt('Give me a number to count down from')
  
   while (count > 0) {
     alert(count + '...')
@@ -15,5 +15,16 @@ function liftOff() {
 
 function make1() {
   // copy and paste your code here when you get to the Make step
-  
+  let count = 0
+  let Maxnumber = prompt('Give me a maximum number to count to')
+  let increase = prompt('Give me a an increment')
+  count = Number(count)
+  increase = Number(increase)
+
+  while (count<Maxnumber) {
+    alert(count + '...')
+    count = count+increase  
+  }
+   
+  alert('Lift Off!')
 }
