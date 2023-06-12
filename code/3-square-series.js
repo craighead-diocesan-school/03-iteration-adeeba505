0a3efe6f0a4edb3427789e231c6cc2c62dd85939
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-12 03:24:49 +0000

    finished square series

--- code/3-square-series.js
+++ code/3-square-series.js
@@ -4,4 +4,13 @@
 
 function squareSeries() {
   // write your code here
+  let maxSquare = prompt('What number do you want the Square Series for?')
+  let count = 1 
+  let series = ''
+
+  while(count <= maxSquare) {
+    series = series + count * count + ','
+    count = count + 1
+  }
+  alert('Series for ' + maxSquare + ' is: ' + series)
 }
