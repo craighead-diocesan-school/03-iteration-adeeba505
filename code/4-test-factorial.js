86a1e056e624f3dcb4fd634a1e5d4e39a1e2a1c8
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-22 22:50:17 +0000

    finished sqaure series

--- code/4-test-factorial.js
+++ code/4-test-factorial.js
@@ -4,4 +4,13 @@
 
 function factorial() {
   // write your code here
+  let factorial = prompt('Gimme the number to the factorial')
+  let count = factorial
+  let total = 1
+
+  while (count > 1) {
+    total = total * count
+    count = count - 1
+  }
+  alert('The factorial of ' + factorial +' is ' + total)
 }
